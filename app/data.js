4feda082da83eff7d7a9673841c3e5d78985f410
Endel Dreyer <endel.dreyer@gmail.com>
2015-07-12 22:19:47 -0300

    first commit.

---
+++ app/data.js
@@ -0,0 +1,162 @@
+{
+  "github": [{
+    "id": "hook",
+    "title": "hook",
+    "description": "Backend as a Service",
+    "repository": "doubleleft/hook"
+  }, {
+    "id": "js2php",
+    "title": "js2php",
+    "description": "JavaScript to PHP Transpiler",
+    "repository": "endel/js2php"
+  }, {
+    "id": "joyjs",
+    "title": "joy.js",
+    "description": "HTML5 2D Game Engine",
+    "repository": "endel/joy.js"
+  }, {
+    "id": "generator-modern-frontend",
+    "title": "generator-modern-frontend",
+    "description": "Yeoman Generator",
+    "repository": "endel/generator-modern-frontend"
+  }],
+
+  "work": [{
+    "id": "fundacaolemann-masterbrand",
+    "title": "Fundação Lemann Website",
+    "client": "fundacao-lemann",
+    "year": "2015",
+    "company": "Fundação Lemann",
+    "description": "<p>A WordPress custom theme. Here I've developed a custom module system and automatic social media importing. It is very easy to use for the end-user to create and associate new modules to it's pages.</p>"
+
+  },{
+    "id": "redbull-hino",
+    "title": "Hino Red Bull Brasil",
+    "client": "redbull",
+    "year": "2015",
+    "company": "Red Bull",
+    "video": {
+      "url": "//www.youtube.com/embed/ig0GDeM6C2A",
+      "source": "youtube",
+      "thumbnail": "http://img.youtube.com/vi/ig0GDeM6C2A/2.jpg"
+    },
+    "description": "<p>A collaborative hymn composition environment for Red Bull's brazilian soccer team.</p><p>Awards and recognition: <a href=\"http://www.frenchdesignindex.com/design-index-9482\">French Design Index</a>, <a href=\"http://www.csswinner.com/details/hino-red-bull/8834\">CSS Winner</a>, <a href=\"http://www.cssdesignawards.com/sites/hino-red-bull/26088/\">CSS Design Awards</a>, <a href=\"http://www.designlicks.com/website/hino-red-bull-21141\">Design Licks</a>, <a href=\"http://www.onepageawards.com/website/hino-red-bull/\">One Page Awards</a>, among other sites and bookmarks.</p><p>Technology stack: PHP, MySQL, HTML5</p>"
+
+  }, {
+    "id": "google-wcdashboard",
+    "title": "Google Word Cup Dashboard",
+    "client": "google",
+    "year": "2014",
+    "company": "Google",
+    "description": "<p>The goal was to create an environment which would keep the Google Doodler's team aware of everything that happened in the World Cup. Technology stack: Phantomjs for scraping and saving Instagram screenshots, Ruby, Rails, and Async Message-queue Processing for social media data import.</p>"
+
+  },{
+    "id": "fiat-bravorama",
+    "title": "Bravorama",
+    "client": "fiat",
+    "year": "2014",
+    "company": "Fiat",
+    "video": {
+      "url": "//player.vimeo.com/video/115393192",
+      "source": "vimeo",
+      "thumbnail": "https://i.vimeocdn.com/video/501413653_200x150.jpg"
+    },
+    "description": "<p>Bravorama was an interactive installation for \"Salão do Automóvel\" in São Paulo - Brazil. I was responsible to integrate physical controllers with the game and to make the cars move through the trail. Technology stack: Unity3d, Serial communication and Python - an Autodesk Maya script to extract trail points for the game.</p>"
+
+  },{
+    "id": "cheetos-kungfu",
+    "title": "Cheetos: Kung-fu",
+    "client": "cheetos",
+    "year": "2014",
+    "company": "Pepsi-co",
+    "description": "<p>A rock-paper-scissors style game. I was responsible for the real-time PvP communication and backend services. Technology stack: PHP, WebSockets, HTML5</p>"
+
+  },{
+    "id": "hellmanns-gwu",
+    "title": "Grow With Us",
+    "client": "hellmanns",
+    "year": "2014",
+    "company": "Unilever",
+    "video": {
+      "url": "//player.vimeo.com/video/116864907",
+      "source": "vimeo",
+      "thumbnail": "https://i.vimeocdn.com/video/503364525_200x150.jpg"
+    },
+    "description": "<p>A real-time campaign where users can follow the sustainable farming process via a livestream feed from cameras placed in the farmer’s hats. For ten days, visitors could interact with the farmers who grow the tomatoes, get to know them, and ask questions. Technology stack: PHP, HTML5</p>"
+
+  },{
+    "id": "axe-throneoflove",
+    "title": "Axe: Throne of Love",
+    "client": "axe",
+    "year": "2013",
+    "company": "Unilever",
+    "video": {
+      "url": "//player.vimeo.com/video/90461908",
+      "source": "vimeo",
+      "thumbnail": "https://i.vimeocdn.com/video/469604038_200x150.jpg"
+    },
+    "description": "Which is the most popular family name in Brazil? In this campaign we've indexed the number of friends, checkins, and all the data we could extract from user's Facebook, and provide a real-time analysis of that. Technology stack: PHP, Laravel, MySQL."
+
+  },{
+    "id": "app-globeleza",
+    "title": "App Globeleza",
+    "client": "globo",
+    "year": "2013",
+    "company": "Globo",
+    "video___TODO": {
+      "url": "http://appglobeleza.g1.globo.com/assets/chamada_globeleza.mp4",
+      "source": "mp4",
+      "thumbnail": "https://i.vimeocdn.com/video/469604038_200x150.jpg"
+    },
+    "description": "<p>I was responsible for the back-end and administrator implementation. Technology stack: PHP, MongoDB</p>"
+
+  }],
+
+  "jams": [{
+    "id": "nimble-forge",
+    "title": "Nimble Forge",
+    "client": "ludum-dare-32",
+    "year": "2015",
+    "video": {
+      "url": "//www.youtube.com/embed/XRLAF6ryXnQ",
+      "source": "youtube",
+      "thumbnail": "http://img.youtube.com/vi/XRLAF6ryXnQ/2.jpg"
+    },
+    "description": "<p>This was the most fun game-jam ever since. We built it in 72h for Ludum Dare 32. Technology stack: JavaScript, pixi.js</p>"
+
+  }, {
+    "id": "dark-light",
+    "title": "Dark Light",
+    "company": "SPJam",
+    "client": "spjam",
+    "year": "2014",
+    "video": {
+      "url": "//www.youtube.com/embed/dY7zEjS0wC8",
+      "source": "youtube",
+      "thumbnail": "http://img.youtube.com/vi/dY7zEjS0wC8/2.jpg"
+    },
+    "description": "<p>We built it in 48h for SPJam. Technology stack: JavaScript, pixi.js</p>"
+
+  },{
+    "id": "corabell",
+    "title": "Corabell",
+    "client": "global-game-jam",
+    "company": "Global Game Jam",
+    "year": "2013",
+    "description": "<p>We built it in 48h for Global Game Jam. Technology stack: JavaScript, phaser.js</p>"
+
+  },{
+    "id": "rufus",
+    "title": "Rufus",
+    "client": "global-game-jam",
+    "company": "Global Game Jam",
+    "year": "2012",
+    "description": "<p>Rufus and The Magic Mushrooms was the first game-jam I've ever participated. We've built it in 48h using Flash and the Flixel engine.</p>",
+    "video": {
+      "url": "//www.youtube.com/embed/KiJ0AkPyu9s",
+      "source": "youtube",
+      "thumbnail": "http://img.youtube.com/vi/KiJ0AkPyu9s/2.jpg"
+    }
+
+  }]
+}
